511ef7535a0da870ba7adb83e05c31edd2e20fc3
Gabriel <gclluch@gmail.com>
2019-05-21 13:54:01 -0700

    first

---
+++ app.js
@@ -0,0 +1,48 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const date = require(__dirname + "/date.js");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+let items = ["test"];
+let workItems = [];
+
+app.get("/", function(req, res) {
+  const day = date.getDate();
+  res.render("list", { listTitle: day, items: items });
+});
+
+//add item and redirect home
+app.post("/", function(req, res) {
+
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  }
+  else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work List", items: workItems});
+});
+
+app.post("/work", function(req,res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.listen(3000, function() {
+  console.log('Server has started on port 3000');
+});
